d6549d340b0946ea6e040fb99822e5f7343dfc24
FluffEzic <42347002+Fluffezic@users.noreply.github.com>
2023-06-23 10:06:57 +0200

    uploaded 1st time

---
+++ examen-oop-juni-Yenthe/src/main/java/helloWorld/Playlist.java
@@ -0,0 +1,63 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "Track")
+public class Playlist {
+	
+	@Id
+	@Column(name = "Trackid")
+	private Integer Trackid;
+	@Column(name = "Name")
+	private String Name;
+	@Column(name = "Albumid")
+	private Integer Albumid;
+	@Column(name = "MediaTypeid")
+	private Integer MediaTypeid;
+	@Column(name = "Genreid")
+	private Integer Genreid;
+	@Column(name = "Composer")
+	private String Composer;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+
+	public void setId(Integer Id) {
+		this.id = Id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+	
+	
+	
+}
